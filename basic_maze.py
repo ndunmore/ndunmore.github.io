502cc8af43da8455365c4ee0dd757a9107a80e59
ndunmore <ndunmore@kippnyccp.org>
2015-05-20 13:10:12 -0400

    Update basic_maze.py

--- basic_maze.py
+++ basic_maze.py
@@ -1,1 +1,14 @@
+from gopigp import * #impot all functions from the gopi libary
+import time #allows you to use the time.sleep() function
+import math #allow you to use math functions like floor()
+
+set_speed(100) #you can use values from 0 to 255
+
+enc_tgt(1,1)
+time.sleep(.1)
+fwd() 
+time.sleep(3)
+
+def move_forward ():
+  
 
